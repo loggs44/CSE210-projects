11be1d4225e5aaebfafb3afcdca05a7c4ba00364
Logan Clark <cla20067@byui.edu>
2023-01-31 11:09:50 -0700

    Learning 3 1/31/23

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -5,27 +5,27 @@ class Fraction{
 
     private int _bottom;
 
-    //Constructor
+    //Constructor to set variables equal to one.
     public Fraction(){
         _top = 1;
         _bottom = 1;
     }
-
+    //Constructor to set top number as a whole number.
     public Fraction(int wholeNumber){
         _top = wholeNumber;
         _bottom = 1;
     }
-
+    //Getter and Setter for the top an bottom numbers.
     public Fraction(int top, int bottom){
         _top = top;
         _bottom = bottom;
     }
-
+    //Method to return the number as a fraction.
     public string GetFractionString(){
         string text = $"{_top}/{_bottom}";
         return text;
     }
-
+    //Method to return the number in decimal form.
     public double GetDecimalValue(){
         return (double)_top / (double)_bottom;
     }
